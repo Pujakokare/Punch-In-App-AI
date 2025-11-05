499ca2ed3bfa607b01fb818ee3ef2222a1024bb3
Pujakokare <puja.sonur7@gmail.com>
2025-11-05 11:38:13 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const container = document.getElementById('root');
-createRoot(container).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
