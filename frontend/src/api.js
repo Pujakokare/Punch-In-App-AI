97e36a07c4460d3d859e64927f15ef5478e2c376
Pujakokare <puja.sonur7@gmail.com>
2025-11-05 10:13:07 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_URL || "";
+const API_BASE = process.env.REACT_APP_API_URL
+  ? `${process.env.REACT_APP_API_URL}/api`
+  : "/api";
 
 export async function fetchPunches() {
   try {
